2077e332bae1059eba8e7cf3606da92ee098f561
Tomas Marquez <btomasmarquez@gmail.com>
2023-05-22 22:10:14 -0300

    API calls creation I'm finishing the APIs call with dynamic content depending the ip retrievied from the client, to call the weather api

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const router = express.Router();
 const axios = require('axios');
-const http = require('node:http');
 const { SuperfaceClient } = require('@superfaceai/one-sdk');
 
-app.enable('trust proxy');
+require('dotenv').config();
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/', async (req,res,next) =>{
-  const ip = req.ip;
-  console.log(ip);
-  next();
-})
+
+async function fetchData(city) {
+  try {
+    const apiKey = process.env.WEATHER_KEY;
+    const city_query = 'Montevideo' //city string to call the lunar phase
+    const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city_query}&aqi=no`);
+    const { data } = response;
+    //console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 // You can manage tokens here: https://superface.ai/insights
 const sdk = new SuperfaceClient({ sdkAuthToken: 'sfs_3695a2325c1a9f353bf6ea2432932a60141cfbe7346e0760be93b0284e4a76c89ffb275bfc9cba0b626976a0f7513789073b2dc37d1c01c56f2425162a377767_55415deb' });
 
 async function run(ip) {
+
   // Load the profile
   const profile = await sdk.getProfile('address/ip-geolocation@1.0.1');
 
@@ -27,7 +37,7 @@ async function run(ip) {
   const result = await profile
     .getUseCase('IpGeolocation')
     .perform({
-      ipAddress: '186.55.248.220'
+      ipAddress: ip
     }, {
       provider: 'ipdata',
       security: {
@@ -41,57 +51,50 @@ async function run(ip) {
   try {
     const data = result.unwrap();
     console.log(data);
+    return data;
   } catch (error) {
     console.error(error);
   }
 }
 
-run();
+async function fetchMultipleData(ip) {
+  var arrayOfResults = [];
+  const ipResult = await fetchData();
+  arrayOfResults.push(ipResult)
+
+  const locationinfo = await run(ip);
+  arrayOfResults.push(locationinfo)
+
+  console.log(arrayOfResults)
+  return arrayOfResults;
+
+}
+
+app.get('/location', async (req, res, next) => {
+
+  const clientIP = req.connection.remoteAddress
+  const ip = clientIP.slice(7)
+  try {
+    const information = await fetchMultipleData(ip);
+    res.send(information)
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error')
+  }
+})
 
 
+router.get('/', async (req, res) => {
 
+});
 
-/*router.get('/', function (req, res) {
-
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-  //__dirname : It will resolve to your project folder.
-
-});*/
+router.get('/public/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+})
 
 //add the router
 app.use('/', router);
 app.use(express.static(__dirname + '/public'));
-
-/*http.get('http://api.weatherapi.com/v1/current.json?key=6d32a8daddf9426ea9732451232704&q=London&aqi=no', (res) => {
-  const { statusCode } = res;
-  const contentType = res.headers['content-type'];
-
-  res.setEncoding('utf8');
-  let rawData = '';
-  res.on('data', (chunk) => { rawData += chunk; });
-  res.on('end', () => {
-    try {
-      const parsedData = JSON.parse(rawData);
-      console.log(parsedData);
-    } catch (e) { console.error(e.message) }
-  })
-}) */
-
-async function callbackFunction () {
-  try {
-  
-    const response = await axios.get('http://api.weatherapi.com/v1/current.json?key=6d32a8daddf9426ea9732451232704&q=London&aqi=no');
-    const { data } = response;
-    console.log(data);
-  }catch(error){
-    console.error(error.message)
-
-}};
-callbackFunction();
-
-
-
-
 
 
 
