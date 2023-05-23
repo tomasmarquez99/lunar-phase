8bdb64a490dece2d300aab8b54da1c4904e61066
Tomas Marquez <btomasmarquez@gmail.com>
2023-05-22 23:22:53 -0300

    functions configuration to retrieve Ip and city the functions are correctly developed, but since i cannot change the Ip from where I try to make the call and double check that the functions are working properly, I'm investigating other ways to implement this.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 async function fetchData(city) {
   try {
     const apiKey = process.env.WEATHER_KEY;
-    const city_query = 'Montevideo' //city string to call the lunar phase
+    const city_query = city //city string to call the lunar phase
     const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city_query}&aqi=no`);
     const { data } = response;
     //console.log(data);
@@ -28,21 +28,23 @@ async function fetchData(city) {
 // You can manage tokens here: https://superface.ai/insights
 const sdk = new SuperfaceClient({ sdkAuthToken: 'sfs_3695a2325c1a9f353bf6ea2432932a60141cfbe7346e0760be93b0284e4a76c89ffb275bfc9cba0b626976a0f7513789073b2dc37d1c01c56f2425162a377767_55415deb' });
 
-async function run(ip) {
+async function run() {
 
   // Load the profile
   const profile = await sdk.getProfile('address/ip-geolocation@1.0.1');
+
+const apiKey_ipdata = process.env.IPDATA_KEY
 
   // Use the profile
   const result = await profile
     .getUseCase('IpGeolocation')
     .perform({
-      ipAddress: ip
+      ipAddress: '179.25.67.101'
     }, {
       provider: 'ipdata',
       security: {
         apikey: {
-          apikey: '400f8681c5367108f6179a04f11457cade1c52ae4d545d0e0c779e23'
+          apikey: apiKey_ipdata
         }
       }
     });
@@ -50,22 +52,25 @@ async function run(ip) {
   // Handle the result
   try {
     const data = result.unwrap();
-    console.log(data);
     return data;
   } catch (error) {
     console.error(error);
   }
 }
 
-async function fetchMultipleData(ip) {
+async function fetchMultipleData() {
   var arrayOfResults = [];
-  const ipResult = await fetchData();
-  arrayOfResults.push(ipResult)
 
-  const locationinfo = await run(ip);
+  const locationinfo = await run();
   arrayOfResults.push(locationinfo)
 
-  console.log(arrayOfResults)
+  const city = arrayOfResults[0].addressRegion
+
+  console.log(city)
+
+  const ipResult = await fetchData(city);
+  arrayOfResults.push(ipResult)
+  
   return arrayOfResults;
 
 }
@@ -75,7 +80,7 @@ app.get('/location', async (req, res, next) => {
   const clientIP = req.connection.remoteAddress
   const ip = clientIP.slice(7)
   try {
-    const information = await fetchMultipleData(ip);
+    const information = await fetchMultipleData();
     res.send(information)
   } catch (error) {
     console.error(error);
